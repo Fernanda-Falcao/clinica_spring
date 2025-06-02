d7d196fd0b7fc3b9f0c8bb007e1ce09da5ba81b5
davi gerson <dgas1@discente.ifpe.edu.br>
2025-06-02 14:04:35 -0300

    ate agora tudo criado

--- src/main/java/com/br/spring_security/clinica/model/Perfil.java
+++ src/main/java/com/br/spring_security/clinica/model/Perfil.java
@@ -1,19 +1,30 @@
 package com.br.spring_security.clinica.model;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+@SuppressWarnings("serial")
 @Entity
 @Table(name = "perfis")
 @Data
-@NoArgsConstructor
-@EqualsAndHashCode(callSuper = true) // Considera o ID herdado no equals/hashCode
+@NoArgsConstructor // Construtor sem argumentos (requerido por JPA)
+@EqualsAndHashCode(callSuper = true) // Considera o ID da superclasse no equals/hashCode
 public class Perfil extends AbstractEntity {
 
+	// Nome único do perfil, ex: "ROLE_ADMIN"
 	@Column(name = "descricao", nullable = false, unique = true)
-	private String desc; // Nome do perfil (ex: ADMIN, MEDICO, PACIENTE)
+	private String descricao;
 
+	// Construtor com ID para uso rápido (ex: associação manual)
 	public Perfil(Long id) {
-		super.setId(id); // Construtor rápido com ID
+		super.setId(id);
+	}
+
+	// Construtor completo (opcional, pode ajudar em testes ou seeds)
+	public Perfil(Long id, String descricao) {
+		super.setId(id);
+		this.descricao = descricao;
 	}
 }
