2a6411db028445dba0d542f546853bff8e782087
davi gerson <dgas1@discente.ifpe.edu.br>
2025-06-02 14:24:02 -0300

    ate agora tudo criado

--- src/main/java/com/br/spring_security/clinica/ClinicaApplication.java
+++ src/main/java/com/br/spring_security/clinica/ClinicaApplication.java
@@ -2,11 +2,13 @@ package com.br.spring_security.clinica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @SpringBootApplication
 public class ClinicaApplication {
-
 	public static void main(String[] args) {
+		System.out.println(new BCryptPasswordEncoder().encode("admin"));
 		SpringApplication.run(ClinicaApplication.class, args);
 	}
 
