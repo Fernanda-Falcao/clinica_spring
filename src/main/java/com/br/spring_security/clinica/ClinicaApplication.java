884ecfe050608009e859e99d7bfcd3007ce61bf4
davi gerson <dgas1@discente.ifpe.edu.br>
2025-06-01 21:29:05 -0300

    agora foi no postgres

---
+++ src/main/java/com/br/spring_security/clinica/ClinicaApplication.java
@@ -0,0 +1,13 @@
+package com.br.spring_security.clinica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaApplication.class, args);
+	}
+
+}
